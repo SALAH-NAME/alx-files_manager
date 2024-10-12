ec00f90df2c5599b66900f823ddfe9a72313518a
Salah <Salah>
2024-10-12 16:51:02 +0100

    files

---
+++ controllers/UsersController.js
@@ -0,0 +1,31 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const emailExists = await dbClient.db.collection('users').findOne({ email });
+    if (emailExists) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = sha1(password);
+    const newUser = { email, password: hashedPassword };
+
+    const result = await dbClient.db.collection('users').insertOne(newUser);
+    const userId = result.insertedId;
+
+    return res.status(201).json({ id: userId, email });
+  }
+}
+
+export default UsersController;
