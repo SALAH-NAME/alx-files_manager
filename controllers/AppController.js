cab10df08d520c7e5c270cc82d294a4202a5bfe2
Salah <Salah>
2024-10-12 16:48:44 +0100

    files

---
+++ controllers/AppController.js
@@ -0,0 +1,24 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+  static getStatus(req, res) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    res.status(200).json(status);
+  }
+
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    const stats = {
+      users,
+      files,
+    };
+    res.status(200).json(stats);
+  }
+}
+
+export default AppController;
